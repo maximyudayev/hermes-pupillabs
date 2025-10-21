ef5e437ee854180da9636a64fc7487e4cd0b547a
Maxim Yudayev <maxim.yudayev@gmail.com>
2025-10-21 17:28:08 +0200

    feat: runnable namespace package

---
+++ examples/pupilcore.bat
@@ -0,0 +1,1 @@
+call hermes-cli -o .\ --config_file pupilcore.yml --experiment project=Test type=PupilCore trial=0
---
+++ examples/pupilcore.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hermes-cli -o ./ --config_file pupilcore.yml --experiment project=Test type=PupilCore trial=0
